f0a7db424a7f3e81c4048d086184474959b602aa
Jack Wrenn <me@jswrenn.com>
2017-03-12 12:30:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This crate provides the `Final`, struct which guarantees the interior
+immutability of the value that it wraps. This is useful for
+preserving invariants on the fields of structures, whose 'safe'
+mutation would cause undefined behavior.
---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use std::ops::Deref;
+
+/// This crate provides the `Final`, struct which guarantees the interior
+/// immutability of the value that it wraps. This is useful for
+/// preserving invariants on the fields of structures, whose 'safe'
+/// mutation would cause undefined behavior.
+
+// https://www.reddit.com/r/rust/comments/26jzm5/immutable_struct_members_in_rust/chs3ljz/
+
+/// Wraps a value in the `Final` type, which does not give out mutable
+/// references.
+#[derive(Debug)]
+pub struct Final<T>(T);
+
+impl<T> Final<T> {
+  /// Wrap a value `v` in a `Final`
+  #[inline]
+  pub fn new(v: T) -> Final<T> {
+    Final(v)
+  }
+}
+
+impl<T> Deref for Final<T> {
+  type Target = T;
+
+  fn deref(&self) -> &T {
+    &self.0
+  }
+}
