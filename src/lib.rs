a2dbfd623863cc6b5963021ec11373d35da42945
Momo Langenstein <momo.langenstein17@imperial.ac.uk>
2021-08-09 08:03:47 +0000

    Formatted the library and README

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@
 use core::ops::Deref;
 
 /// This no-std crate provides the `Final`, struct which guarantees the interior
-/// immutability of the value that it wraps. This is useful for
-/// preserving invariants on the fields of structures, whose 'safe'
-/// mutation would cause undefined behavior.
+/// immutability of the value that it wraps. This is useful for preserving
+/// invariants on the fields of structures, whose 'safe' mutation would cause
+/// undefined behavior.
 
 // https://www.reddit.com/r/rust/comments/26jzm5/immutable_struct_members_in_rust/chs3ljz/
 
@@ -16,17 +16,17 @@ use core::ops::Deref;
 pub struct Final<T>(T);
 
 impl<T> Final<T> {
-  /// Wrap a value `v` in a `Final`
-  #[inline]
-  pub fn new(v: T) -> Final<T> {
-    Final(v)
-  }
+    /// Wrap a value `v` in a `Final`
+    #[inline]
+    pub fn new(v: T) -> Final<T> {
+        Final(v)
+    }
 }
 
 impl<T> Deref for Final<T> {
-  type Target = T;
+    type Target = T;
 
-  fn deref(&self) -> &T {
-    &self.0
-  }
+    fn deref(&self) -> &T {
+        &self.0
+    }
 }
